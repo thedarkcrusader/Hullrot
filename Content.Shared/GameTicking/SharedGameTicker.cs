0baa01e49e12cbae4635c68e73754d0eeb8b6517
Eris <eris@erisws.com>
2025-05-20 15:32:12 -0400

    guh

--- Content.Shared/GameTicking/SharedGameTicker.cs
+++ Content.Shared/GameTicking/SharedGameTicker.cs
@@ -6,6 +6,9 @@ using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.Markdown.Mapping;
 using Robust.Shared.Serialization.Markdown.Value;
 using Robust.Shared.Timing;
+using Robust.Shared.Utility;
+using Robust.Shared.Prototypes;
+using Content.Shared.Shipyard.Prototypes;
 
 namespace Content.Shared.GameTicking
 {
@@ -133,20 +136,75 @@ namespace Content.Shared.GameTicking
         }
     }
 
+    // Frontier: station job info, optional structs
+    /// <summary>
+    /// General job information for each station-like entity (both stations and shuttles)
+    /// </summary>
+    /// <param name="stationName">The name of the station.</param>
+    /// <param name="jobsAvailable">A dictionary of job prototypes and the number of jobs positions available for it.</param>
+    /// <param name="isLateJoinStation">If true, this entity is a station, and not a player ship (displayed under the "Crew" tab).</param>
     [Serializable, NetSerializable]
-    public sealed class TickerJobsAvailableEvent : EntityEventArgs
+    public sealed class StationJobInformation(
+        string stationName,
+        Dictionary<ProtoId<JobPrototype>, int?> jobsAvailable,
+        bool isLateJoinStation,
+        StationDisplayInformation? stationDisplayInfo,
+        VesselDisplayInformation? vesselDisplayInfo
+        )
     {
-        /// <summary>
-        /// The Status of the Player in the lobby (ready, observer, ...)
-        /// </summary>
-        public Dictionary<NetEntity, Dictionary<string, uint?>> JobsAvailableByStation { get; }
-        public Dictionary<NetEntity, string> StationNames { get; }
+        public string StationName { get; } = stationName;
+        public Dictionary<ProtoId<JobPrototype>, int?> JobsAvailable { get; } = jobsAvailable;
+        public bool IsLateJoinStation { get; } = isLateJoinStation;
+        public StationDisplayInformation? StationDisplayInfo { get; } = stationDisplayInfo;
+        public VesselDisplayInformation? VesselDisplayInformation { get; } = vesselDisplayInfo;
+    }
 
-        public TickerJobsAvailableEvent(Dictionary<NetEntity, string> stationNames, Dictionary<NetEntity, Dictionary<string, uint?>> jobsAvailableByStation)
-        {
-            StationNames = stationNames;
-            JobsAvailableByStation = jobsAvailableByStation;
-        }
+    /// <summary>
+    /// Additional optional station-specific fields.
+    /// </summary>
+    /// <param name="stationSubtext">The subtext that is shown under the station name.</param>
+    /// <param name="stationDescription">A longer description of the station, describing what the player can
+    /// do there</param>
+    /// <param name="stationIcon">The icon that represents the station and is shown next to the name.</param>
+    /// <param name="lobbySortOrder">The order in which this station should be displayed in the station picker.</param>
+    [Serializable, NetSerializable]
+    public sealed class StationDisplayInformation(
+        LocId? stationSubtext,
+        LocId? stationDescription,
+        ResPath? stationIcon,
+        int lobbySortOrder
+        )
+    {
+        public LocId? StationSubtext { get; } = stationSubtext;
+        public LocId? StationDescription { get; } = stationDescription;
+        public ResPath? StationIcon { get; } = stationIcon;
+        public int LobbySortOrder { get; } = lobbySortOrder;
+    }
+
+    /// <summary>
+    /// Additional optional vessel-specific fields.
+    /// </summary>
+    /// <param name="vesselAdvertisement">A player-input string advertising the ship to other players.</param>
+    /// <param name="vessel">The prototype ID for the vessel this ship is.</param>
+    /// <param name="hiddenIfNoJobs">If true, this vessel should be hidden when there are no open jobs on it.</param>
+    [Serializable, NetSerializable]
+    public sealed class VesselDisplayInformation(
+        string vesselAdvertisement,
+        ProtoId<VesselPrototype>? vessel,
+        bool hiddenIfNoJobs
+        )
+    {
+        public string VesselAdvertisement { get; } = vesselAdvertisement;
+        public ProtoId<VesselPrototype>? Vessel { get; } = vessel;
+        public bool HiddenIfNoJobs { get; } = hiddenIfNoJobs;
+    }
+    // End Frontier: station job info, optional structs
+
+    public sealed class TickerJobsAvailableEvent(
+        Dictionary<NetEntity, StationJobInformation> stationJobList // Frontier addition, replaced with StationJobInformation
+    ) : EntityEventArgs
+    {
+        public Dictionary<NetEntity, StationJobInformation> StationJobList { get; } = stationJobList;
     }
 
     [Serializable, NetSerializable, DataDefinition]
